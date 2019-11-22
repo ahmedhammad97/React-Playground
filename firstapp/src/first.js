0c938db118a26c7fe285c6eb4fd1625cd911d619
Ahmed Hammad <hammad97official@gmail.com>
2019-11-23 00:55:27 +0200

    Nest some components

---
+++ firstapp/src/first.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FirstComponent() {
+  return (
+    <div className="FirstComponent">
+      <h1>What a heading!</h1>
+    </div>
+  );
+}
+
+export default FirstComponent;
---
+++ firstapp/src/second.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SecondComponent() {
+  return (
+    <div className="SecondComponent">
+      <p>Here's a simple plain paragraph</p>
+    </div>
+  );
+}
+
+export default SecondComponent;
