2e5da0be95b6fc13cbb7176bda166d3b350ddc44
Ahmed Hammad <hammad97official@gmail.com>
2019-11-23 16:04:19 +0200

    Add comments for lifting state up

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -13,4 +13,12 @@ function App() {
   );
 }
 
+// In order to sync the state of multiple components,
+// we need to keep this state in a common ancestor.
+// Which will pass a "control function" to their props.
+// Then, all the components can use this "control function"
+// to mutate the state.
+// In the parent component, setState might execute in an async
+// so we must do it in the setState function form.
+
 export default App;
